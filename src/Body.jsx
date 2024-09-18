24907649aa43ced3c1f0f4d86842e957371e116c
rap-el <20237896@s.ubaguio.edu>
2024-09-18 15:55:52 +0800

    Add files via upload

--- src/Body.jsx
+++ src/Body.jsx
@@ -8,23 +8,21 @@ export default function Body(props) {
                 alt={`${props.name}'s picture`} 
                 className="person-picture"
             />
-            <p>      This is <strong>{props.name}'s</strong> <strong>{props.state}</strong> <strong>{props.thing}</strong>: <strong>{props.content}</strong></p>
+            <p><strong>{props.name}</strong></p>
+            <p><strong>{props.content}</strong></p>
+            
         </div>
     );
 }
 
 Body.defaultProps = {
     name: "Guest",
-    state: "Unknown",
-    thing: "Unknown",
-    content: "Unknown",
+    content: "Guest",
     imageUrl: "https://placeholder.pics/svg/150x150", 
 };
 
 Body.propTypes = {
     name: PropTypes.string,
-    state: PropTypes.string,
-    thing: PropTypes.string,
     content: PropTypes.string,
     imageUrl: PropTypes.string, 
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,12 @@
 export default function Header() {
     return (
         <header>
-            <center><h1>MY PROFILE</h1></center>
+            <center><h1>GUILLAUME</h1></center>
             <nav>
                 <ul>
-                    {}
+                    <li><a href="">KB X1000</a>   </li>
+                    <li><a href="">SPECS</a>   </li>
+                    
                 </ul>
             </nav>
         </header>
